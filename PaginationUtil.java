d04adbf592971f2dbaeb3e50458cd1851500abae
xabiib <xabiib@gmail.com>
2024-09-07 12:22:28 -0400

    spring boot files

---
+++ PaginationUtil.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2016-2024 the original author or authors from the JHipster project.
+ *
+ * This file is part of the JHipster project, see https://www.jhipster.tech/
+ * for more information.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package tech.jhipster.web.util;
+
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.util.UriComponentsBuilder;
+
+/**
+ * Utility class for handling pagination.
+ *
+ * <p>
+ * Pagination uses the same principles as the <a href="https://docs.github.com/rest/guides/using-pagination-in-the-rest-api">GitHub API</a>,
+ * and follow <a href="http://tools.ietf.org/html/rfc5988">RFC 5988 (Link header)</a>.
+ */
+public final class PaginationUtil {
+
+    private static final String HEADER_X_TOTAL_COUNT = "X-Total-Count";
+
+    private static LinkHeaderUtil linkHeaderUtil = new LinkHeaderUtil();
+
+    private PaginationUtil() {}
+
+    /**
+     * Generate pagination headers for a Spring Data {@link org.springframework.data.domain.Page} object.
+     *
+     * @param uriBuilder The URI builder.
+     * @param page The page.
+     * @param <T> The type of object.
+     * @return http header.
+     */
+    public static <T> HttpHeaders generatePaginationHttpHeaders(UriComponentsBuilder uriBuilder, Page<T> page) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HEADER_X_TOTAL_COUNT, Long.toString(page.getTotalElements()));
+        headers.add(HttpHeaders.LINK, linkHeaderUtil.prepareLinkHeaders(uriBuilder, page));
+        return headers;
+    }
+}
